29dd7fbc37055484bff789cef8908dbd22c6083f
chizuki <kritsu@qq.com>
2023-04-06 17:55:05 +0800

    feat: support write.

--- src/index.ts
+++ src/index.ts
@@ -1,157 +1,322 @@
-
-
-
-
-export type MemoryBufferHandlers = {
-	[key: string]: (buffer: MemoryBuffer, ...args: any[]) => any
+export interface MemoryBufferHandlers {
+  [key: string]: (buffer: MemoryBuffer, ...args: any[]) => any
 }
 
+export interface MemoryBufferDefineObject {
+  [key: string]: MemoryBufferDefineType
+}
+
+export type MemoryBufferDefineType = string | number | [string, ...any[]] | MemoryBufferDefineObject | ((buffer: MemoryBuffer) => any);
 export interface MemoryBuffer extends Record<string, any> {
-	readonly offset: number;
-	readonly length: number;
-	[Symbol.iterator](): Iterator<number>;
+  readonly offset: number
+  readonly length: number
+  readonly buffer: Uint8Array
+  readonly bufferChunkSize: number
 
-	skip(length: number, start?: number): this;
-	resume(start?: number): this;
+  [Symbol.iterator](): Iterator<number>
 
-	readNumber(length: number): number;
-	readUNumber(length: number): number;
-	readString(length: number): string;
-	readBytes(length: number): Uint8Array;
-	readObject<T extends Record<string, any> = MemoryBufferDefine>(define: T): any
-	readInt(): number;
-	readUInt(): number;
-	readShort(): number;
-	readUShort(): number;
-	readByte(): number;
-	readUByte(): number;
-	readLong(): number;
-	readULong(): number;
+  skip(length: number, start?: number): this
+  resume(start?: number): this
+  clear(): this
 
+  readNumber(length: number, signed?: boolean): number
+  readString(length?: number): string
+  readBytes(length: number): Uint8Array
+  readObject<T extends Record<string, any> = MemoryBufferDefineObject>(define: T): any
+  readInt(signed?: boolean): number
+  readShort(signed?: boolean): number
+  readByte(signed?: boolean): number
+  readLong(signed?: boolean): number
+  readArray(length: number, define: MemoryBufferDefineType): any[]
+
+  writeNumber(value: number, length: number, signed?: boolean): this
+  writeString(value: string, length?: number): this
+  writeBytes(value: Uint8Array): this
+  writeObject<T extends Record<string, any> = MemoryBufferDefineObject>(value: T, define: T): this
+  writeInt(value: number, signed?: boolean): this
+  writeShort(value: number, signed?: boolean): this
+  writeByte(value: number, signed?: boolean): this
+  writeLong(value: number, signed?: boolean): this
+  writeArray(value: any[], length: number, define: MemoryBufferDefineType): this
+
+  defineRead(defineRecord: MemoryBufferDefineType): () => any
+  defineWrite(defineRecord: MemoryBufferDefineType): (value: any) => this
 }
 
 // 将字节数组转换为无符号的数字,正序读
 function bytesToUNumber(bytes: Uint8Array) {
-	return bytes.reduce((n, b, i) => n | b * (1 << (i * 8)), 0);
+  return bytes.reduce((n, b, i) => n | b * (1 << (i * 8)), 0);
 }
 
+function numberToBytes(n: number, length: number) {
+  const bytes = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    bytes[i] = n & 0xFF;
+    n = n >> 8;
+  }
+  return bytes;
+}
 
 // 将字节数组转换为带符号的数字
-function bytesToNumber(bytes: Uint8Array) {
-	const n = bytesToUNumber(bytes);
-	const sign = 1 << (bytes.length * 8 - 1);
-	return (n ^ sign) - sign;
+function toSignedNumber(n: number) {
+  return n > 0x7FFFFFFF ? n - 0x100000000 : n;
+}
+
+function toUnsignedNumber(n: number) {
+  return n < 0 ? n + 0x100000000 : n;
 }
 
 // 将字节数组转换为字符串
 function bytesToString(bytes: Uint8Array) {
-	return String.fromCharCode(...bytes).replace(/\0+$/g, '');
+  return String.fromCharCode(...bytes).replace(/\0+$/g, "");
 }
 
+// 将字符串转换为字节数组
+function stringToBytes(str: string, length: number) {
+  const bytes = new Uint8Array(length);
+  for (let i = 0; i < length; i++) {
+    if (i >= str.length) {
+      break;
+    }
+    bytes[i] = str.charCodeAt(i) || 0;
+  }
+  return bytes;
+}
 
 // 驼峰式命名
 function toCamelCase(str: string) {
-	return str.replace(/[_-][a-z]/g, (match) => {
-		return match[1].toUpperCase();
-	});
+  return str.replace(/[_-][a-z]/g, (match) => {
+    return match[1].toUpperCase();
+  });
 }
 
-export type MemoryBufferDefine = {
-	[key: string]: number | string | MemoryBufferDefine | [string, ...any[]] | ((buffer: MemoryBuffer) => any)
+export interface MemoryBufferOptions {
+  startOffset?: number
+  signedNumber?: boolean
+  bufferChunkSize?: number
+  memoizeDefine?: ((...args: any[]) => any) | boolean
 }
 
-export function createMemoryBuffer(bytes: Iterable<number>): MemoryBuffer {
+function sizeOf<T>(iterable: Iterable<T>) {
+  let size = 0;
+  for (const _ of iterable) {
+    size++;
+  }
+  return size;
+}
 
-	const buffer = new Uint8Array(bytes);
+export function createMemoryBuffer(bytes: Iterable<number> = [], options: MemoryBufferOptions = {}): MemoryBuffer {
+  let _buffer = new Uint8Array(bytes);
 
-	let _offset = 0;
+  const _bufferChunkSize = options.bufferChunkSize ?? 128;
 
-	return {
-		get offset() {
-			return _offset;
-		},
-		get length() {
-			return buffer.length;
-		},
-		[Symbol.iterator]() {
-			return buffer[Symbol.iterator]();
-		},
-		skip(length: number, start: number = -1) {
-			if (start >= 0) {
-				_offset = start;
-			}
-			_offset += length;
-			return this;
-		},
-		resume(start = -1) {
-			_offset = start >= 0 ? start : 0;
-			return this;
-		},
-		readNumber(length: number) {
-			const rs = bytesToNumber(buffer.subarray(_offset, _offset + length));
-			this.skip(length);
-			return rs;
-		},
-		readUNumber(length: number) {
-			const rs = bytesToUNumber(buffer.subarray(_offset, _offset + length));
-			this.skip(length);
-			return rs;
-		},
-		readInt() {
-			return this.readNumber(4);
-		},
-		readUInt() {
-			return this.readUNumber(4);
-		},
-		readShort() {
-			return this.readNumber(2);
-		},
-		readUShort() {
-			return this.readUNumber(2);
-		},
-		readByte() {
-			return this.readNumber(1);
-		},
-		readUByte() {
-			return this.readUNumber(1);
-		},
-		readLong() {
-			return this.readNumber(8);
-		},
-		readULong() {
-			return this.readUNumber(8);
-		},
-		readString(length: number) {
-			const rs = bytesToString(buffer.subarray(_offset, _offset + length));
-			this.skip(length);
-			return rs;
-		},
-		readBytes(length: number) {
-			const rs = buffer.subarray(_offset, _offset + length);
-			this.skip(length);
-			return rs;
-		},
-		readObject<T extends object = MemoryBufferDefine>(this: MemoryBuffer, define: T) {
-			const rs: Record<string, any> = {};
-			for (let key in define) {
-				const value = define[key];
-				if (typeof value === 'number') {
-					rs[key] = this.readBytes(value);
-				} else if (typeof value === "string") {
-					const methodName = toCamelCase(`read_${value}`);
-					rs[key] = this[methodName as keyof MemoryBuffer]();
-				} else if (Array.isArray(value)) {
-					const [type, ...args] = value;
-					const methodName = toCamelCase(`read_${type}`);
-					rs[key] = this[methodName as keyof MemoryBuffer](...args);
-				} else if (typeof value === 'object') {
-					rs[key] = this.readObject(value as MemoryBufferDefine);
-				} else if (typeof value === 'function') {
-					rs[key] = value(this);
-				}
-			}
-			return rs as ReturnType<MemoryBuffer['readObject']>;
-		}
-	}
+  let _length = sizeOf(bytes);
 
+  let _offset = options.startOffset ?? 0;
+
+  const memoizeDefine = options.memoizeDefine ?? true;
+
+  const memoize = function<P extends any[], R>(fn: (...args: any[]) => any) {
+    if (!memoizeDefine) {
+      return fn;
+    }
+
+    const cache = new Map();
+    return function (this: any, ...args: P) {
+      const key = typeof memoizeDefine === "function" ? memoizeDefine(...args) : args;
+      if (cache.has(key)) {
+        return cache.get(key) as R;
+      }
+      const value = fn.call(this, ...args);
+      cache.set(key, value);
+      return value;
+    };
+  };
+
+  return {
+    get offset() {
+      return _offset;
+    },
+    get length() {
+      return _length;
+    },
+    get bufferChunkSize() {
+      return _bufferChunkSize;
+    },
+    get buffer() {
+      return _buffer;
+    },
+    [Symbol.iterator]() {
+      return _buffer[Symbol.iterator]();
+    },
+    skip(length: number, start = -1) {
+      if (start >= 0) {
+        _offset = start;
+      }
+      _offset += length;
+      return this;
+    },
+    resume(start = -1) {
+      _offset = start >= 0 ? start : 0;
+      return this;
+    },
+    toUint8Array() {
+      return _buffer;
+    },
+    readNumber(length: number, signed = options.signedNumber) {
+      const bytes = this.readBytes(length);
+      let value = bytesToUNumber(bytes);
+      if (signed) {
+        value = toSignedNumber(value);
+      }
+      return value;
+    },
+    readInt(signed) {
+      return this.readNumber(4, signed);
+    },
+    readShort(signed) {
+      return this.readNumber(2, signed);
+    },
+    readByte(signed) {
+      return this.readNumber(1, signed);
+    },
+    readLong(signed) {
+      return this.readNumber(8, signed);
+    },
+    readString(length?: number) {
+      if (!length) {
+        length = _buffer.findIndex((b, i) => i > _offset && b === 0);
+      }
+      if (length < 0) {
+        length = _buffer.length - _offset;
+      }
+      const bytes = this.readBytes(length);
+      return bytesToString(bytes);
+    },
+    readBytes(length: number) {
+      const rs = _buffer.subarray(_offset, _offset + length);
+      this.skip(length);
+      return rs;
+    },
+    defineRead: memoize(function (this: MemoryBuffer, defineRecord: string | number | [string, ...any[]] | MemoryBufferDefineObject) {
+      const isBytes = typeof defineRecord === "number";
+      const isString = typeof defineRecord === "string";
+      const isArgs = Array.isArray(defineRecord);
+      const isObject = !isArgs && typeof defineRecord === "object" && defineRecord !== null;
+      const handler = () => {
+        if (isBytes) {
+          return this.readBytes(defineRecord);
+        } else if (isString) {
+          const methodName = toCamelCase(`read_${defineRecord}`);
+          return this[methodName as keyof MemoryBuffer]();
+        } else if (isArgs) {
+          const [type, ...args] = defineRecord;
+          const methodName = toCamelCase(`read_${type}`);
+          return this[methodName as keyof MemoryBuffer](...args);
+        } else if (isObject) {
+          return this.readObject(defineRecord);
+        }
+      };
+      return handler;
+    }),
+    defineWrite: memoize(function (this: MemoryBuffer, defineRecord: string | number | [string, ...any[]] | MemoryBufferDefineObject) {
+      const isBytes = typeof defineRecord === "number";
+      const isString = typeof defineRecord === "string";
+      const isArgs = Array.isArray(defineRecord);
+      const isObject = !isArgs && typeof defineRecord === "object" && defineRecord !== null;
+      const handler = (value: any) => {
+        if (isBytes) {
+          this.writeBytes(value);
+        } else if (isString) {
+          const methodName = toCamelCase(`write_${defineRecord}`);
+          this[methodName as keyof MemoryBuffer](value);
+        } else if (isArgs) {
+          const [type, ...args] = defineRecord;
+          const methodName = toCamelCase(`write_${type}`);
+          this[methodName as keyof MemoryBuffer](value, ...args);
+        } else if (isObject) {
+          this.writeObject(value, defineRecord);
+        }
+        return this;
+      };
+      return handler;
+    }),
+
+    readObject<T extends object = MemoryBufferDefineObject>(this: MemoryBuffer, define: T) {
+      const rs: Record<string, any> = {};
+      for (const key in define) {
+        const value = define[key] as MemoryBufferDefineType;
+        rs[key] = this.defineRead(value)();
+      }
+      return rs as ReturnType<MemoryBuffer["readObject"]>;
+    },
+    readArray(this: MemoryBuffer, define: MemoryBufferDefineType, length: number) {
+      const rs: any[] = [];
+      const handler = this.defineRead(define);
+      for (let i = 0; i < length; i++) {
+        rs.push(handler());
+      }
+      return rs;
+    },
+    writeNumber(value: number, length: number, signed = options.signedNumber) {
+      if (signed) {
+        value = toUnsignedNumber(value);
+      }
+      const bytes = numberToBytes(value, length);
+      return this.writeBytes(bytes);
+    },
+    writeInt(value: number, signed = options.signedNumber) {
+      return this.writeNumber(value, 4, signed);
+    },
+    writeShort(value: number, signed = options.signedNumber) {
+      return this.writeNumber(value, 2, signed);
+    },
+    writeByte(value: number, signed = options.signedNumber) {
+      return this.writeNumber(value, 1, signed);
+    },
+    writeLong(value: number, signed = options.signedNumber) {
+      return this.writeNumber(value, 8, signed);
+    },
+    writeString(str: string, length?: number) {
+      const bytes = stringToBytes(str, length ?? str.length);
+      this.writeBytes(bytes);
+      return this;
+    },
+    writeBytes(bytes: Iterable<number>) {
+      const array = Array.isArray(bytes) ? bytes : [...bytes];
+      const oldLength = _buffer.length;
+      const newLength = _offset + array.length;
+
+      if (newLength > oldLength) {
+        const oldBuffer = _buffer;
+        // 扩充buffer
+        _buffer = new Uint8Array(Math.ceil(newLength / _bufferChunkSize) * _bufferChunkSize);
+        _buffer.set(oldBuffer);
+        _length = newLength;
+      }
+      _buffer.set(array, _offset);
+      _offset = newLength;
+      return this;
+    },
+    writeObject<T extends object = MemoryBufferDefineObject>(this: MemoryBuffer, obj: T, define: T) {
+      for (const key in define) {
+        const value = define[key] as MemoryBufferDefineType;
+        this.defineWrite(value)(obj[key]);
+      }
+      return this;
+    },
+    writeArray(this: MemoryBuffer, arr: any[], define: MemoryBufferDefineType) {
+      const handler = this.defineWrite(define);
+      for (const item of arr) {
+        handler(item);
+      }
+      return this;
+    },
+    clear() {
+      _offset = 0;
+      _buffer = new Uint8Array(0);
+      return this;
+    }
+
+  };
 }
--- test/index.test.ts
+++ test/index.test.ts
@@ -1,28 +1,45 @@
-import { createMemoryBuffer } from "../src/index";
 import { describe, expect, it } from "vitest";
+import { createMemoryBuffer } from "../src/index";
 
-describe("data", async () => {
-	it("mock-data", async () => {
-		const buffer = createMemoryBuffer([
-			0x02, 0x00, 0x00, 0x00,
-			0x01, 0x00, 0x00, 0x00,
-			0x03, 0x00, 0x00, 0x00,
-		]);
+describe("test", async () => {
+  it("run-test", async () => {
+    const buffer = createMemoryBuffer([], {
+    });
 
+    buffer.writeString("hello world");
 
-		expect(buffer.readInt()).toBe(2);
-		expect(buffer.readInt()).toBe(1);
-		expect(buffer.readInt()).toBe(3);
-		buffer.resume();
-		expect(buffer.readLong()).toBe(3);
+    buffer.resume();
 
-		buffer.resume();
-		const obj = buffer.readObject({
-			a: "int",
-			b: "int",
-		});
-		expect(obj.a).toBe(2);
-		expect(obj.b).toBe(1);
+    const str = buffer.readString();
 
-	});
+    expect(str).toBe("hello world");
+
+    buffer.clear();
+    buffer.writeInt(1);
+    buffer.writeInt(2);
+    buffer.writeInt(3);
+    buffer.writeLong(4);
+    buffer.resume();
+
+    expect(buffer.readInt()).toBe(1);
+    expect(buffer.readInt()).toBe(2);
+    expect(buffer.readInt()).toBe(3);
+    expect(buffer.readLong()).toBe(4);
+
+    buffer.resume();
+
+    const obj = buffer.readObject({
+      a: "int",
+      b: "int",
+      c: {
+        d: "int",
+        e: "int"
+      }
+    });
+
+    expect(obj.a).toBe(1);
+    expect(obj.b).toBe(2);
+    expect(obj.c.d).toBe(3);
+    expect(obj.c.e).toBe(4);
+  });
 });
